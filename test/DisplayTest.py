59e1438d7731add3d775f8e9e56c558f366a60c2
Darryl Richman <darryl@crafty-fox.com>
2021-07-11 16:20:02 -0700

    Add host IP to display test

--- test/DisplayTest.py
+++ test/DisplayTest.py
@@ -3,7 +3,21 @@ sys.path.append('..')
 import Display
 from datetime import datetime
 import time
+import socket
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+host = get_ip()
 d = Display.Display()
 d.clear()
 
@@ -18,5 +32,8 @@ for i in range(3):
     d.draw.text((10,174), 'wind WNW', font=d.font24, fill=0)
     d.draw.text((10,200), ' 10/25g mph', font=d.font24, fill=0)
     d.draw.text((10,226), 'rain 0.00 in', font=d.font24, fill=0)
+
+    width, height = d.get_canvas_size(d.DISP_LAYOUT_PORTRAIT)
+    d.draw.text((0,height - 26), host, font=d.font24, fill=0)
     d.finish_drawing()
     time.sleep(10)
