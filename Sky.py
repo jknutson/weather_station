f6a69861c9564d5487b42d8249418038c784caae
Darryl Richman <darryl@crafty-fox.com>
2021-07-22 15:51:52 -0700

    Fix selection process for pom names

--- Sky.py
+++ Sky.py
@@ -3,26 +3,36 @@ from astral.sun import sun
 from astral import moon
 import datetime
 
-phases = {
-    (0, 0.5): 'New Moon',
-    (0.5, 6.5): 'Waxing Crescent',
-    (6.5, 7.5): 'First Quarter',
-    (7.5, 13.5): 'Waxing Gibbous',
-    (13.5, 14.5): 'Full Moon',
-    (14.5, 20.5): 'Waining Gibbous',
-    (20.5, 21.5): 'Last Quarter',
-    (21.5, 27.5): 'Waining Crescent',
-    (27.5, 29): 'New Moon'
-}
+def get_phase(date):
+    points = {
+        7: 'First Quarter',
+        14: 'Full Moon',
+        21: 'Last Quarter',
+    }
+    ranges = {
+        (0, 7): 'Waxing Crescent',
+        (7, 14): 'Waxing Gibbous',
+        (14, 21): 'Waining Gibbous',
+        (21, 28): 'Waining Crescent',
+    }
+    phase = moon.phase(date)
+    for k, name in points.items():
+        tomorrow = moon.phase(date + datetime.timedelta(days=1))
+        if tomorrow < phase:
+            return 'New Moon'
+        if k >= phase and k < tomorrow:
+            return name
+    for k, name in ranges.items():
+        s,e = k
+        if phase >= s and phase < e:
+            return name
+    return False
 
 location = LocationInfo("Santa Cruz", "United States", "America/Los_Angeles", 37.014835, -121.979731)
 sun_state = sun(location.observer, date=datetime.date.today(), tzinfo=location.timezone)
 sunrise = sun_state["sunrise"].strftime("%H:%M:%S")
 sunset = sun_state["sunset"].strftime("%H:%M:%S")
-phase = moon.phase(datetime.date.today())
-for k, name in phases.items():
-    s,e = k
-    if phase >= s and phase <= e:
-        pom = name
-        break
+my_date = datetime.datetime.now()
+phase = moon.phase(my_date)
+pom = get_phase(my_date)
 print('sunrise: '+sunrise+', sunset: '+sunset+', POM: '+pom+', Moon Day: '+str(phase))
