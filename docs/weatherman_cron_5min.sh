d24a970b8f5e1c80a578074117f8a48db017a1fc
john.knutson@hennepin.us <john.knutson@hennepin.us>
2023-07-17 16:33:25 -0500

    feat: add cron script

--- docs/weatherman_cron_5min.sh
+++ docs/weatherman_cron_5min.sh
@@ -4,4 +4,4 @@ echo "select wm_update_latest_readings();" | psql -h localhost -U jknutson -d mn
 # echo "\copy (select json_agg(wm_latest_readings)->0 from wm_latest_readings where station='pico' limit 1) to stdout quote '$'" | psql -h localhost -U jknutson -d mn-gis-projects > /var/www/html/current_conditions.json
 echo "select json_agg(wm_latest_readings)->0 from wm_latest_readings where station='pico' limit 1" | psql -t -h localhost -U jknutson -d mn-gis-projects 2>/dev/null > /var/www/html/current_conditions.json
 cat /var/www/html/current_conditions.json
-curl -X PUT -d "@/var/www/html/current_conditions.json" -H "x-ms-date: $(TZ=GMT date '+%a, %d %h %Y %H:%M:%S %Z')" -H "x-ms-blob-type: BlockBlob" "https://weatherman.blob.core.windows.net/%24web/current_conditions.json${SAS_TOKEN}"
+curl -s -X PUT -d "@/var/www/html/current_conditions.json" -H "x-ms-date: $(TZ=GMT date '+%a, %d %h %Y %H:%M:%S %Z')" -H "x-ms-blob-type: BlockBlob" "https://weatherman.blob.core.windows.net/%24web/current_conditions.json${SAS_TOKEN}"
