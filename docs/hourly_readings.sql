8a5ebd30efb59fde5b847d0f51481cf16de36f94
john.knutson@hennepin.us <john.knutson@hennepin.us>
2023-07-17 13:52:41 -0500

    feat: add min/max temp and humidity to hourly

--- docs/hourly_readings.sql
+++ docs/hourly_readings.sql
@@ -3,8 +3,13 @@ CREATE TABLE wm_hourly_readings (
 id varchar(100) unique primary key not null,
 hour timestamp,
 avg_temperature float,
+min_temperature float,
+max_temperature float,
 avg_humidity float,
+min_humidity float,
+max_humidity float,
 wind_speed_gust_mph float,
+max_wind_speed_gust_mph float,
 wind_speed_avg_mph float,
 wind_speed_direction varchar(5),
 rainfall_hour float,
@@ -15,6 +20,10 @@ station varchar(50)
 create unique index idx_wm_hourly_readings_id on wm_hourly_readings (id);
 create unique index idx_wm_hourly_readings_id_hour on wm_hourly_readings (id, hour);
 alter table wm_hourly_readings add column station varchar(50);
+alter table wm_hourly_readings add column min_temperature float;
+alter table wm_hourly_readings add column max_temperature float;
+alter table wm_hourly_readings add column min_humidity float;
+alter table wm_hourly_readings add column max_humidity float;
 */
 
 /*
@@ -43,12 +52,14 @@ CREATE OR REPLACE FUNCTION public.wm_update_hourly_summaries(
 AS $BODY$
 
 -- TEMPERATURE
-INSERT INTO wm_hourly_readings (id, hour, station, avg_temperature)
+INSERT INTO wm_hourly_readings (id, hour, station, avg_temperature, min_temperature, max_temperature)
 SELECT
 	SPLIT_PART(topic, '/', 2)||'_'||to_char(date_trunc('hour', time::timestamptz AT TIME ZONE 'cdt'), 'YYYYMMDDHH24') as id,
 	date_trunc('hour', time::timestamptz AT TIME ZONE 'cdt') as hour,
 	SPLIT_PART(topic, '/', 2) as station,
-	avg(text::float) AS avg_temperature
+	avg(text::float) AS avg_temperature,
+        min(text::float) AS min_temperature,
+        max(text::float) AS max_temperature
 FROM journal
 WHERE 
 	topic = 'iot/pico/temperature_avg'
@@ -58,22 +69,24 @@ GROUP BY 1,2,3
 ORDER BY 1,2
 ON CONFLICT(id, hour)
 DO UPDATE
-SET avg_temperature = EXCLUDED.avg_temperature;
+SET avg_temperature = EXCLUDED.avg_temperature, min_temperature = EXCLUDED.min_temperature, max_temperature = EXCLUDED.max_temperature;
 
 -- HUMIDITY
-INSERT INTO wm_hourly_readings (id, hour, station, avg_humidity)
+INSERT INTO wm_hourly_readings (id, hour, station, avg_humidity, min_humidity, max_humidity)
 SELECT
 	SPLIT_PART(topic, '/', 2)||'_'||to_char(date_trunc('hour', time::timestamptz AT TIME ZONE 'cdt'), 'YYYYMMDDHH24') as id,
 	date_trunc('hour', time::timestamptz AT TIME ZONE 'cdt') as hour,
 	SPLIT_PART(topic, '/', 2) as station,
-	avg(text::float) AS avg_humidity
+	avg(text::float) AS avg_humidity,
+        min(text::float) AS min_humidity,
+        max(text::float) AS max_humidity
 FROM journal
 WHERE 
 	topic = 'iot/pico/humidity_avg'
 	AND text::float < 118 AND text::float > -118 -- weed out outliers/erroneous readings
 	AND date_trunc('hour', time::timestamptz AT TIME ZONE 'cdt') = date_trunc('hour', current_timestamp AT TIME ZONE 'cdt' - INTERVAL '1 hour')-- previous hour
 GROUP BY 1,2,3 ORDER BY 1,2
-ON CONFLICT(id, hour) DO UPDATE SET avg_humidity = EXCLUDED.avg_humidity;
+ON CONFLICT(id, hour) DO UPDATE SET avg_humidity = EXCLUDED.avg_humidity, min_humidity = EXCLUDED.min_humidity, max_humidity = EXCLUDED.max_humidity;
 
 -- WIND AND RAIN
 INSERT INTO wm_hourly_readings (id, hour, station, rainfall_hour, rainfall_day, wind_speed_avg_mph, wind_speed_gust_mph, wind_speed_direction)
@@ -99,9 +112,7 @@ ON CONFLICT(id, hour) DO UPDATE SET
   wind_speed_gust_mph = EXCLUDED.wind_speed_gust_mph,
   wind_speed_direction = EXCLUDED.wind_speed_direction;
 
-$BODY$;
-
-/*
+$BODY$;/*
 select wm_update_hourly_summaries();
 select mode() WITHIN GROUP (ORDER BY data->'wind'->'direction') AS wind_speed_direction from journal;
 select * from journal where left(topic,3)='iot' order by journal_id desc limit 15;
